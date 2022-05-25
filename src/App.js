1879a8c89395d09fbb6bd71a6c3382a8ee773637
Fred Coutinho <fredcoutinho52@gmail.com>
2022-05-25 11:30:46 -0300

    removendo input e texto

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ function App() {
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <p>Texto</p>
-
-      <label htmlFor="nome">Nome</label>
-      <input type="text" id="nome"/>
     </div>
   );
 }
