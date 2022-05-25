52bae5948a5f14aa93f2e7ea37dcfb28f575844c
Fred Coutinho <fredcoutinho52@gmail.com>
2022-05-25 11:20:57 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <p>Texto</p>
+
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome"/>
+    </div>
+  );
+}
+
+export default App;
